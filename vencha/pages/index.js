f80ba0f6b24e3fedb103cf10c95e30be90327394
rokajova <35878122+rokajova@users.noreply.github.com>
2021-05-24 04:59:34 +0300

    vecha

--- vencha/pages/index.js
+++ vencha/pages/index.js
@@ -1,8 +1,42 @@
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
-import CreatePost from "../components/CreatePost";
-import Main from "../components/Main";
+// This component represents the index page for the site. You
+// can read more about Pages in the Next.js docs at:
+// https://nextjs.org/docs/basic-features/pages
 
-export default function Home() {
-  return <div>Yo</div>;
+import { getPosts } from "../config/firebase";
+
+const HomePage = ({ posts }) => (
+  <div>
+    <h1>Blog Posts</h1>
+    {posts.map((post) => (
+      <article key={post.slug}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
+        <div>
+          <h2>{post.title}</h2>
+          <span>{getFormattedDate(post.dateCreated)}</span>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: `${post.content.substring(0, 200)}...`,
+            }}
+          ></p>
+        </div>
+      </article>
+    ))}
+  </div>
+);
+
+// This is for fetching data every time the page is visited. We do this
+// so that we don't have to redploy the site every time we add a blog post.
+// You can read more about this in the Next.js docs at:
+// https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+
+export async function getServerSideProps() {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
+
+export default HomePage;
