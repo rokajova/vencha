c2f41dd484fdaec3e9a4e59733d8b65675a17777
rokajova <35878122+rokajova@users.noreply.github.com>
2021-05-31 15:29:38 +0300

    create project using different guidelines

--- vencha/pages/index.js
+++ vencha/pages/index.js
@@ -1,43 +1,14 @@
 import { useEffect, useState } from "react";
 import firebase from "../config/firebase";
+import Head from "next/head";
 
-export default function Main({ posts }) {
+export default function Main() {
   return (
     <div>
-      <h1>All Posts</h1>
-      {posts.map((post, index) => (
-        <div style={{ border: "1px solid black", margin: "10px" }} key={index}>
-          {post.title}, {post.content}
-          <a href={`/posts/${posts.slug}`}>Continue Reading</a>
-        </div>
-      ))}
+      <Head>
+        <title>Vencha</title>
+      </Head>
+      <h1>Blog</h1>
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  let posts = [];
-  try {
-    // await the promise
-    const querySnapshot = await firebase.firestore().collection("blog").get();
-
-    // "then" part after the await
-    querySnapshot.forEach(function (doc) {
-      posts.push({
-        content: doc.data().content,
-        title: doc.data().title,
-      });
-    });
-    console.log(posts);
-  } catch (error) {
-    // catch part using try/catch
-    console.log("Error getting documents: ", error);
-    // return something else here, or an empty props, or throw an exception or whatever
-  }
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
