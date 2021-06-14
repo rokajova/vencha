ac11c5971d3d791dcd2445f084e786216624a7aa
rokajova <35878122+rokajova@users.noreply.github.com>
2021-06-14 16:34:49 +0300

    media upload

--- vencha/components/MediaUpload.js
+++ vencha/components/MediaUpload.js
@@ -1,4 +1,11 @@
+import React, { useState } from "react";
+
 const MediaUpload = () => {
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState("");
+
   return (
     <div>
       <input type="file" />
