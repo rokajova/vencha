ec52ed631517a69c2e2985b76fa2fbb9ad8e8e12
rokajova <35878122+rokajova@users.noreply.github.com>
2021-07-16 21:43:20 +0300

    Layout
    New layout with new components folder.

--- vencha/components/Navbar.js
+++ vencha/components/Navbar.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>as</div>;
+  return (
+    <nav>
+      {/* <div className="logo">
+        <Image src="/atlas.png" width={120} height={70} />
+      </div> */}
+
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/people">
+        <a>People List</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
