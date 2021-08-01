d53a888d77434d09863202d0d4715dff4eaf5de3
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-01 07:48:07 +0300

    Update Layout.js

--- vencha/components/Layout.js
+++ vencha/components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Modal from "./Modal";
 
 const Layout = ({ children }) => {
   return (
     <div className="content">
       <Navbar />
       {children}
+      <Modal />
     </div>
   );
 };
