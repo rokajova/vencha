0b048dda94d7fc044e976da2766b123d61c36688
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-01 20:15:52 +0300

    Collapsible form
    Collapsible

--- vencha/components/Collapsible.js
+++ vencha/components/Collapsible.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import styles from "../styles/Collapsible.module.css";
 
-function Collapsible() {
+function Collapsible(props) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={styles.collapsible}>
       <button className={styles.toggle} onClick={() => setIsOpen(!isOpen)}>
-        Toggle
+        {props.label}
       </button>
-      {isOpen && <div className={styles.content}>Collapsible</div>}
+      {isOpen && <div className={styles.content}>{props.children}</div>}
     </div>
   );
 }
--- vencha/components/Layout.js
+++ vencha/components/Layout.js
@@ -5,9 +5,34 @@ const Layout = ({ children }) => {
   return (
     <div className="content">
       <Navbar />
-      <Collapsible />
-      <Collapsible />
-      <Collapsible />
+      <Collapsible label="Collapsible">
+        <h2>Collapsible</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          similique aliquid nostrum a, iusto cumque aut laboriosam autem.
+          Quaerat, officia.
+        </p>
+      </Collapsible>
+      <Collapsible label="Expand!">
+        <ul>
+          <li>List!</li>
+          <li>List!</li>
+          <li>List!</li>
+          <li>List!</li>
+          <li>List!</li>
+        </ul>
+      </Collapsible>
+      <Collapsible label="🎁">
+        <form>
+          <h3>Title</h3>
+          <input type="text" />
+          <h3>Content</h3>
+          <textarea />
+          <h3>Image or Video</h3>
+          <input type="file" />
+          <button>CREATE</button>
+        </form>
+      </Collapsible>
       {children}
     </div>
   );
