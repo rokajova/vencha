ca86994261e1300b4455cd7c12f07ceb405c3510
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-01 21:28:15 +0300

    Collapsible animation with useREf

--- vencha/components/Collapsible.js
+++ vencha/components/Collapsible.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "../styles/Collapsible.module.css";
 
 function Collapsible(props) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const parentRef = useRef();
+
+  if (parentRef.current) {
+    console.log(parentRef.current.scrollHeight);
+  }
 
   return (
     <div className={styles.collapsible}>
       <button className={styles.toggle} onClick={() => setIsOpen(!isOpen)}>
         {props.label}
       </button>
-      {isOpen && <div className={styles.content}>{props.children}</div>}
+      <div className={styles.contentParent} ref={parentRef}>
+        <div className={styles.content}>{props.children}</div>
+      </div>
     </div>
   );
 }
