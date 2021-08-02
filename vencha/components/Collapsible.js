eb5238436b9e897d3ee8d3f9592a62cb2e119a45
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-02 10:41:55 +0300

    Update Collapsible.js
    States and refs in collapsible component

--- vencha/components/Collapsible.js
+++ vencha/components/Collapsible.js
@@ -1,10 +1,22 @@
 import React, { useState, useRef } from "react";
+import firebase from "../config/firebase";
+import "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 import styles from "../styles/Collapsible.module.css";
 
 function Collapsible(props) {
+  // state for collapsible managment
   const [isOpen, setIsOpen] = useState(false);
-
+  // warning will be used for file size limit
+  const [isImageWarningOpen, setIsImageWarningOpen] = useState(false);
+  // required states that will be used in a post
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [featureImage, setFeatureImage] = useState("");
+  // used for collapsible height calculation
   const parentRef = useRef();
+  // used for ???
+  const inputEl = useRef();
 
   return (
     <div className={styles.collapsible}>
@@ -22,7 +34,9 @@ function Collapsible(props) {
       >
         <div className={styles.content}>
           <form>
-            <input type="text" />
+            <input type="text" placeholder="Title" />
+            <input type="file" />
+            <input type="text" placeholder="Vent" />
           </form>
         </div>
       </div>
